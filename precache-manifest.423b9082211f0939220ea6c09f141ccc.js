80a99504e3ef10249c3e94c1e1dd965c21f8765a
Romano Krychkovskyy <kruchkovskiy2@gmail.com>
2019-04-14 20:05:34 +0300

    Updates

---
+++ precache-manifest.423b9082211f0939220ea6c09f141ccc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ef238c531091b074a876115e33c672",
+    "url": "/testin/static/media/chemistry.e2ef238c.png"
+  },
+  {
+    "revision": "a585328a031b35dc7ad7",
+    "url": "/testin/static/css/main.ece05b3a.chunk.css"
+  },
+  {
+    "revision": "fd83ab9c22c560799fbc",
+    "url": "/testin/static/js/runtime~main.6947ce3f.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/testin/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d9d8a13e075b4654c33b76bccbb11e29",
+    "url": "/testin/static/media/header-background.d9d8a13e.png"
+  },
+  {
+    "revision": "51ed81ff9d03d42165676e7a9de68d1f",
+    "url": "/testin/static/media/slide1.51ed81ff.png"
+  },
+  {
+    "revision": "c5b4a362af9ea6b97d4cabbfea300fe9",
+    "url": "/testin/static/media/slide2.c5b4a362.png"
+  },
+  {
+    "revision": "5120f293bdec72db83659d50b38b0ea3",
+    "url": "/testin/static/media/slide3.5120f293.png"
+  },
+  {
+    "revision": "987e947444b0762203ae330cb4961601",
+    "url": "/testin/static/media/slide4.987e9474.png"
+  },
+  {
+    "revision": "b8a167a8bc200db32e8209eb9ed21fc6",
+    "url": "/testin/static/media/physics.b8a167a8.png"
+  },
+  {
+    "revision": "a585328a031b35dc7ad7",
+    "url": "/testin/static/js/main.b567262f.chunk.js"
+  },
+  {
+    "revision": "bf54b9f7e1233809b63784a76c93285a",
+    "url": "/testin/static/media/technology.bf54b9f7.png"
+  },
+  {
+    "revision": "3d44de11f886de818f9d486b30c6b7f1",
+    "url": "/testin/static/media/environment.3d44de11.png"
+  },
+  {
+    "revision": "d5c25cdfa2f412779ed8e08bdabb44b2",
+    "url": "/testin/static/media/space.d5c25cdf.png"
+  },
+  {
+    "revision": "7b5cc4d00589e2b43eb28dc29c95c437",
+    "url": "/testin/static/media/environment.7b5cc4d0.png"
+  },
+  {
+    "revision": "9b8da9c258155d29e233adea52d1adc7",
+    "url": "/testin/static/media/technology.9b8da9c2.png"
+  },
+  {
+    "revision": "a79e882bbed2545121fd5b6e9ada40e6",
+    "url": "/testin/static/media/space.a79e882b.png"
+  },
+  {
+    "revision": "b0d89dbd5be4ae2186ea4b0f6263fc9f",
+    "url": "/testin/static/media/physics.b0d89dbd.png"
+  },
+  {
+    "revision": "38b8b9aefa7695c759b49cfb713a7286",
+    "url": "/testin/static/media/chemistry.38b8b9ae.png"
+  },
+  {
+    "revision": "d503c9e5b96bb535d7c742ca9afe5ef8",
+    "url": "/testin/static/media/brain.d503c9e5.png"
+  },
+  {
+    "revision": "aef961921c9ebe0bce409ac4c2c5e4ce",
+    "url": "/testin/index.html"
+  }
+];
